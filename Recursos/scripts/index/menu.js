089acc5744d1c9ff0d70537d599f976f429bf52d
Maye <mayerlysalas06@gmail.com>
2024-06-28 14:16:56 -0500

    Página__principal

--- Recursos/scripts/index/menu.js
+++ Recursos/scripts/index/menu.js
@@ -1,15 +1,15 @@
- function toggleMenu() {
-            document.getElementById("myDropdown").classList.toggle("show");
-        }
+function toggleMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
-        window.onclick = function(event) {
-            if (!event.target.matches('.dropbtn')) {
-                var dropdowns = document.getElementsByClassName("dropdown-content");
-                for (var i = 0; i < dropdowns.length; i++) {
-                    var openDropdown = dropdowns[i];
-                    if (openDropdown.classList.contains('show')) {
-                        openDropdown.classList.remove('show');
-                    }
-                }
+window.onclick = function(event) {
+    if (!event.target.matches('.menu-icon')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
             }
         }
+    }
+}
