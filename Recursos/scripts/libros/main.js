a997997bd152f58d01fd14b0707b27e15b0ab10b
Maria <julianahenao1510@gmail.com>
2024-07-03 14:23:32 -0500

    Cambios

---
+++ Recursos/scripts/libros/main.js
@@ -0,0 +1,31 @@
+const menuIcon = document.getElementById('menuIcon');
+const mainMenu = document.querySelector('.main-menu');
+
+menuIcon.addEventListener('click', () => {
+  mainMenu.classList.toggle('visible');
+});
+
+var swiper = new Swiper(".books-container-wrappers", {
+  slidesPerView: 4, // Mostrar 4 libros a la vez
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 2, // Mostrar 2 libros en dispositivos móviles
+    },
+    568: {
+      slidesPerView: 3, // Mostrar 3 libros en pantallas pequeñas
+    },
+    768: {
+      slidesPerView: 4, // Mostrar 4 libros en pantallas medianas
+    },
+    968: {
+      slidesPerView: 4, // Mostrar 4 libros en pantallas grandes
+    },
+  },
+ navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
