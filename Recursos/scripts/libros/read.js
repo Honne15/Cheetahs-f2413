0386745a245e9e6ec113829bf7dfa304c5a71be5
Maria <julianahenao1510@gmail.com>
2024-07-09 02:59:22 -0500

    Barra de progreso

--- Recursos/scripts/libros/read.js
+++ Recursos/scripts/libros/read.js
@@ -12,6 +12,18 @@ document.addEventListener('DOMContentLoaded', function() {
             pdfLibro = libro[4];
             console.log('pdfLibro', pdfLibro);
             mostrarPDF(pdfLibro);
+            setupGuardarButton(libro);
+            if (libro.length > 5) {
+                const audioUrl = libro[5] || '';
+                const isPlaylist = audioUrl.includes('spreaker.com/player');
+                if (audioUrl) {
+                    setupAudioPlayer(audioUrl, isPlaylist);
+                } else {
+                    console.log('Este libro no tiene audio asociado.');
+                }
+            } else {
+                console.log('Este libro no tiene audio asociado.');
+            }
         } else {
             console.error('Libro no encontrado');
         }
@@ -24,10 +36,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
         pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
             const pdfViewer = document.getElementById('pdf-viewer');
+            pdfViewer.classList.add('pdf-viewer-centered')
+            
+            pdfViewer.innerHTML = '';
+
             let pageNum = 1;
+            const totalPages = pdfDoc_.numPages;
 
             function renderPage(page) {
-                const scale = 1.5;
+                const scale = 1.3;
                 const viewport = page.getViewport({ scale: scale });
 
                 const canvas = document.createElement('canvas');
@@ -42,7 +59,12 @@ document.addEventListener('DOMContentLoaded', function() {
 
                 page.render(renderContext).promise.then(() => {
                     pdfViewer.appendChild(canvas);
-                    if (pageNum < pdfDoc_.numPages) {
+
+                    if (pageNum === 1) {
+                        setupScrollProgress(pdfViewer, pdfDoc_, totalPages);
+                    }
+
+                    if (pageNum < totalPages) {
                         pageNum++;
                         pdfDoc_.getPage(pageNum).then(renderPage);
                     }
@@ -51,6 +73,51 @@ document.addEventListener('DOMContentLoaded', function() {
 
             pdfDoc_.getPage(pageNum).then(renderPage);
         });
+    }
+
+    // Función para configurar el progreso al hacer scroll
+    function setupScrollProgress(pdfViewer, pdfDoc, totalPages) {
+        const progressBarContainer = document.getElementById('progress-bar-container');
+        const progressBar = document.getElementById('progress-bar');
+
+        window.addEventListener('scroll', function() {
+            const scrollTop = window.scrollY;
+            const documentHeight = pdfViewer.scrollHeight;
+            const windowHeight = window.innerHeight;
+            const scrollProgress = (scrollTop / (documentHeight - windowHeight)) * 100;
+            progressBar.style.width = scrollProgress.toFixed(2) + '%';
+            progressBarContainer.style.display = 'block';
+
+            if (scrollTop + windowHeight >= documentHeight) {
+                progressBarContainer.style.display = 'none';
+            }
+        });
+    }
+
+    // Función para el reproductor de audio
+    function setupAudioPlayer(audioUrl, isPlaylist = false) {
+        const audioPlayerContainer = document.getElementById('audio-player');
+        audioPlayerContainer.classList.add('audio-player-centered');
+        audioPlayerContainer.innerHTML = '';
+
+        if (isPlaylist) {
+            // Crear el código HTML para mostrar la lista de reproducción
+            const iframe = document.createElement('iframe');
+            iframe.classList.add('audio-player-iframe');
+            iframe.src = audioUrl;
+            iframe.width = '100%';
+            iframe.height = '100%';
+            iframe.allow = 'encrypted-media';
+
+            audioPlayerContainer.appendChild(iframe);
+        } else {
+            // Crear el reproductor de audio individual
+            const audio = document.createElement('audio');
+            audio.src = audioUrl;
+            audio.controls = true;
+
+            audioPlayerContainer.appendChild(audio);
+        }
     }
 
     // Al cargar la página, obtener el libro por su ID y mostrarlo
