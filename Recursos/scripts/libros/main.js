756ac9a8bf4d3bee2a0f78391d2e5f4b3f5faeec
Maye <mayerlysalas06@gmail.com>
2024-07-25 13:25:34 -0500

    Subo trabajo terminado

--- Recursos/scripts/libros/main.js
+++ Recursos/scripts/libros/main.js
@@ -16,35 +16,36 @@ menuCate.addEventListener('click', () => {
 
 //Javasript para el carrusel que va en los libros
 const url = '/Recursos/pdfs/';
+const urls = '/Recursos/pdfs/guias/';
 
 const conCategoria = [
-      ['1', '/Recursos/img/img_libros/portada-a-que-te-cojo-raton.webp', '¡A que te cojo ratón!', 'Juego', '/Recursos/pdfs/A_QUE_TE_COJO_RATON.pdf', ''],
-      ['2', '/Recursos/img/img_libros/portada-salud-para-contar.webp', 'Salud para contar', 'Salud', '/Recursos/pdfs/SALUD_PARA_CONTAR.pdf', ''],
-      ['3', '/Recursos/img/img_libros/portada-el-hombre-y-su-cultura.webp', 'El hombre y su cultura', 'Cultura', '/Recursos/pdfs/EL_HOMBRE_Y_SU_CULTURA.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee'],
-      ['4', '/Recursos/img/img_libros/portada-tiempo-de-hacer.webp', 'Tiempo de hacer', 'Hacer', '/Recursos/pdfs/TIEMPO_DE_HACER.pdf', ''],
-      ['5', '/Recursos/img/img_libros/portada-erase-una-vez-en-colombia.webp', 'Érase una vez en Colombia', 'Historia', '/Recursos/pdfs/ERASE_UNA_VEZ_EN_COLOMBIA.pdf', 'https://widget.spreaker.com/player?show_id=5822929&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true'],
-      ['6', '/Recursos/img/img_libros/portada-los-viajes.webp', 'Los viajes del viejo Jacobo', 'Historia', '/Recursos/pdfs/LOS_VIAJES_DEL_VIEJO_JACOBO.pdf', ''],
-      ['7', '/Recursos/img/img_libros/portada-lecturas-fantasticas.webp', 'Lecturas fantásticas', 'Historia', '/Recursos/pdfs/LITERATURA_FANTASTICA.pdf', ''],
-      ['8', '/Recursos/img/img_libros/portada-historias-y-lugares.webp', 'Historias y lugares', 'Historia','/Recursos/pdfs/HISTORIAS_Y_LUGARES.pdf', 'https://widget.spreaker.com/player?show_id=4671213&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true'],
-      ['9', '/Recursos/img/img_libros/portada-del-campo-a-la-mesa.webp', 'Del campo a la mesa', 'Cocina', '/Recursos/pdfs/DEL_CAMPO_A_LA_MESA.pdf', ''],
-      ['10', '/Recursos/img/img_libros/portada-tan-distintos-y-parientes.webp', 'Tan distintos y parientes', 'Cocina', '/Recursos/pdfs/TAN_DISTINTOS_Y_PARIENTES.pdf', ''],
-      ['11', '/Recursos/img/img_libros/portada-planeta-vivo.webp', 'Planeta vivo', 'Animales', '/Recursos/pdfs/PLANETA_VIVO.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee'],
-      ['12', '/Recursos/img/img_libros/portada-los-secretos-de-los-animales.webp', 'Los secretos de los animales', 'Animales', '/Recursos/pdfs/LOS_SECRETOS_DE_LOS_ANIMALES.pdf', ''],
-      ['13', '/Recursos/img/img_libros/portada-entre-cuento-y-cuento.webp', 'Entre cuento y cuento', 'Cuentos', '/Recursos/pdfs/ENTRE_CUENTO_Y_CUENTO.pdf', ''],
-      ['14', '/Recursos/img/img_libros/portada-cuentos-maravillosos.webp', 'Cuentos maravillosos', 'Cuentos', '/Recursos/pdfs/CUENTOS_MARAVILLOSOS.pdf', ''],
-      ['15', '/Recursos/img/img_libros/portada-cuentos-para-contar.webp', 'Cuentos para contar', 'Cuentos', '/Recursos/pdfs/CUENTOS_PARA_CONTAR.pdf', ''],
-      ['16', '/Recursos/img/img_libros/portada-cuentos-y-pasatiempos.webp', 'Cuentos y pasatiempos', 'Cuentos', '/Recursos/pdfs/CUENTOS_Y_PASATIEMPOS.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee'],
-      ['17', '/Recursos/img/img_libros/portada-para-desenredar-enredos.webp', 'Cuentos para desenredar enredos', 'Cuentos', '/Recursos/pdfs/CUENTOS_PARA_DESENREDAR_ENREDOS.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP'],
-      ['18', '/Recursos/img/img_libros/portada-lecturas-para-todos-los-dias.webp', 'Lecturas para todos los días', 'Cuentos', '/Recursos/pdfs/LECTURAS_PARA_TODOS_LOS_DIAS.pdf', 'https://widget.spreaker.com/player?show_id=4421133&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true'],
-      ['19', '/Recursos/img/img_libros/portada-con-los-pelos-de-punta.webp', 'Con los pelos de punta', 'Familia', '/Recursos/pdfs/CON_LOS_PELOS_DE_PUNTA.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP'],
-      ['20', '/Recursos/img/img_libros/portada-la-tierra-el-cielo.webp', 'La Tierra, el cielo y más allá', 'Familia', '/Recursos/pdfs/LA_TIERRA_EL_CIELO_Y_MAS_ALLA.pdf', ''],
-      ['21', '/Recursos/img/img_libros/portada-todo-lo-contrario.webp', 'Todo lo contrario', 'Familia', '/Recursos/pdfs/TODO_LO_CONTRARIO.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP'],
-      ['22', '/Recursos/img/img_libros/portada-mas-claro-no-canta-un-gallo.webp', 'Más claro no canta un gallo', 'Familia', '/Recursos/pdfs/MAS_CLARO_NO_CANTA_UN_GALLO.pdf', ''],
-      ['23', '/Recursos/img/img_libros/portada-calor-de-hogar.webp', 'Calor de hogar', 'Familia', '/Recursos/pdfs/CALOR_DE_HOGAR.pdf', ''],
-      ['24', '/Recursos/img/img_libros/portada-los-primeros-anos.webp', 'Los primeros años', 'Familia', '/Recursos/pdfs/LOS_PRIMEROS_ANOS.pdf', ''],
-      ['25', '/Recursos/img/img_libros/portada-los-secretos-de-las-plantas.webp', 'Los secretos de las plantas', 'Plantas', '/Recursos/pdfs/LOS_SECRETOS_DE_LAS_PLANTAS.pdf', ''],
-      ['26', '/Recursos/img/img_libros/portada-la-finca-viva.webp', 'La finca viva', 'Plantas', '/Recursos/pdfs/LA_FINCA_VIVA.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP'],
-      ['27', '/Recursos/img/img_libros/portada-casa-y-campo.webp', 'La casa y el campo', 'Plantas', '/Recursos/pdfs/LA_CASA_Y_EL_CAMPO.pdf','https://widget.spreaker.com/player?show_id=4421133&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true']
+      ['1', '/Recursos/img/img_libros/portada-a-que-te-cojo-raton.webp', '¡A que te cojo ratón!', 'Juego', '/Recursos/pdfs/A_QUE_TE_COJO_RATON.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_5.pdf'],
+      ['2', '/Recursos/img/img_libros/portada-salud-para-contar.webp', 'Salud para contar', 'Salud', '/Recursos/pdfs/SALUD_PARA_CONTAR.pdf', '', ''],
+      ['3', '/Recursos/img/img_libros/portada-el-hombre-y-su-cultura.webp', 'El hombre y su cultura', 'Cultura', '/Recursos/pdfs/EL_HOMBRE_Y_SU_CULTURA.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee','/Recursos/pdfs/guias/GUIA_DE_TALLERES_2.pdf'],
+      ['4', '/Recursos/img/img_libros/portada-tiempo-de-hacer.webp', 'Tiempo de hacer', 'Hacer', '/Recursos/pdfs/TIEMPO_DE_HACER.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_4.pdf'],
+      ['5', '/Recursos/img/img_libros/portada-erase-una-vez-en-colombia.webp', 'Érase una vez en Colombia', 'Historia', '/Recursos/pdfs/ERASE_UNA_VEZ_EN_COLOMBIA.pdf', 'https://widget.spreaker.com/player?show_id=5822929&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true', ''],
+      ['6', '/Recursos/img/img_libros/portada-los-viajes.webp', 'Los viajes del viejo Jacobo', 'Historia', '/Recursos/pdfs/LOS_VIAJES_DEL_VIEJO_JACOBO.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_5.pdf'],
+      ['7', '/Recursos/img/img_libros/portada-lecturas-fantasticas.webp', 'Lecturas fantásticas', 'Historia', '/Recursos/pdfs/LITERATURA_FANTASTICA.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_3.pdf'],
+      ['8', '/Recursos/img/img_libros/portada-historias-y-lugares.webp', 'Historias y lugares', 'Historia','/Recursos/pdfs/HISTORIAS_Y_LUGARES.pdf', 'https://widget.spreaker.com/player?show_id=4671213&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_1.pdf'],
+      ['9', '/Recursos/img/img_libros/portada-del-campo-a-la-mesa.webp', 'Del campo a la mesa', 'Cocina', '/Recursos/pdfs/DEL_CAMPO_A_LA_MESA.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_6.pdf'],
+      ['10', '/Recursos/img/img_libros/portada-tan-distintos-y-parientes.webp', 'Tan distintos y parientes', 'Cocina', '/Recursos/pdfs/TAN_DISTINTOS_Y_PARIENTES.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_4.pdf'],
+      ['11', '/Recursos/img/img_libros/portada-planeta-vivo.webp', 'Planeta vivo', 'Animales', '/Recursos/pdfs/PLANETA_VIVO.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_2_PLANETA.pdf'],
+      ['12', '/Recursos/img/img_libros/portada-los-secretos-de-los-animales.webp', 'Los secretos de los animales', 'Animales', '/Recursos/pdfs/LOS_SECRETOS_DE_LOS_ANIMALES.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_6.pdf'],
+      ['13', '/Recursos/img/img_libros/portada-entre-cuento-y-cuento.webp', 'Entre cuento y cuento', 'Cuentos', '/Recursos/pdfs/ENTRE_CUENTO_Y_CUENTO.pdf', '', ''],
+      ['14', '/Recursos/img/img_libros/portada-cuentos-maravillosos.webp', 'Cuentos maravillosos', 'Cuentos', '/Recursos/pdfs/CUENTOS_MARAVILLOSOS.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_5.pdf'],
+      ['15', '/Recursos/img/img_libros/portada-cuentos-para-contar.webp', 'Cuentos para contar', 'Cuentos', '/Recursos/pdfs/CUENTOS_PARA_CONTAR.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_4.pdf'],
+      ['16', '/Recursos/img/img_libros/portada-cuentos-y-pasatiempos.webp', 'Cuentos y pasatiempos', 'Cuentos', '/Recursos/pdfs/CUENTOS_Y_PASATIEMPOS.pdf', 'https://open.spotify.com/embed/show/5c3KxZDPh5CioaBDPMf1ee', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_2.pdf'],
+      ['17', '/Recursos/img/img_libros/portada-para-desenredar-enredos.webp', 'Cuentos para desenredar enredos', 'Cuentos', '/Recursos/pdfs/CUENTOS_PARA_DESENREDAR_ENREDOS.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP', ''],
+      ['18', '/Recursos/img/img_libros/portada-lecturas-para-todos-los-dias.webp', 'Lecturas para todos los días', 'Cuentos', '/Recursos/pdfs/LECTURAS_PARA_TODOS_LOS_DIAS.pdf', 'https://widget.spreaker.com/player?show_id=4421133&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_1.pdf'],
+      ['19', '/Recursos/img/img_libros/portada-con-los-pelos-de-punta.webp', 'Con los pelos de punta', 'Familia', '/Recursos/pdfs/CON_LOS_PELOS_DE_PUNTA.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP', ''],
+      ['20', '/Recursos/img/img_libros/portada-la-tierra-el-cielo.webp', 'La Tierra, el cielo y más allá', 'Familia', '/Recursos/pdfs/LA_TIERRA_EL_CIELO_Y_MAS_ALLA.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_6.pdf'],
+      ['21', '/Recursos/img/img_libros/portada-todo-lo-contrario.webp', 'Todo lo contrario', 'Familia', '/Recursos/pdfs/TODO_LO_CONTRARIO.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP', ''],
+      ['22', '/Recursos/img/img_libros/portada-mas-claro-no-canta-un-gallo.webp', 'Más claro no canta un gallo', 'Familia', '/Recursos/pdfs/MAS_CLARO_NO_CANTA_UN_GALLO.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_6.pdf'],
+      ['23', '/Recursos/img/img_libros/portada-calor-de-hogar.webp', 'Calor de hogar', 'Familia', '/Recursos/pdfs/CALOR_DE_HOGAR.pdf', '','/Recursos/pdfs/guias/GUIA_DE_TALLERES_3.pdf'],
+      ['24', '/Recursos/img/img_libros/portada-los-primeros-anos.webp', 'Los primeros años', 'Familia', '/Recursos/pdfs/LOS_PRIMEROS_ANOS.pdf', '', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_3.pdf'],
+      ['25', '/Recursos/img/img_libros/portada-los-secretos-de-las-plantas.webp', 'Los secretos de las plantas', 'Plantas', '/Recursos/pdfs/LOS_SECRETOS_DE_LAS_PLANTAS.pdf', '',''],
+      ['26', '/Recursos/img/img_libros/portada-la-finca-viva.webp', 'La finca viva', 'Plantas', '/Recursos/pdfs/LA_FINCA_VIVA.pdf', 'https://open.spotify.com/embed/show/4pgUCRcRIQNKwPJBLMWxjP', ''],
+      ['27', '/Recursos/img/img_libros/portada-casa-y-campo.webp', 'La casa y el campo', 'Plantas', '/Recursos/pdfs/LA_CASA_Y_EL_CAMPO.pdf','https://widget.spreaker.com/player?show_id=4421133&theme=dark&playlist=show&playlist-continuous=true&chapters-image=true', '/Recursos/pdfs/guias/GUIA_DE_TALLERES_1.pdf']
   ];
 
   let juego = [];
@@ -120,6 +121,17 @@ const conCategoria = [
                   divHijoPorTitulo.appendChild(aEscuchar);
                 }
                 break;
+              case 6:
+                if (element !== '') {
+                  const aGuia = document.createElement('a');
+                  aGuia.href = '/leerguia.html?id=' + id;
+                  aGuia.target = '_blank';
+                  aGuia.innerHTML = '<i class="fa-solid fa-chalkboard-user"></i> Guía';
+                  aGuia.classList.add('guia-link'); // Clase para aplicarle el estilo
+                  divHijoPorTitulo.appendChild(aGuia);
+                }
+                break;
+
               default:
                 break;
           }
@@ -246,6 +258,17 @@ function filterBooksByCategory(category) {
               divHijoPorTitulo.appendChild(aEscuchar);
             }
             break;
+          case 6:
+            if (element !== '') {
+              const aGuia = document.createElement('a');
+              aGuia.href = '/leerguia.html?id=' + id;
+              aGuia.target = '_blank';
+              aGuia.innerHTML = '<i class="fa-solid fa-chalkboard-user"></i> Guía';
+              aGuia.classList.add('guia-link'); // Clase para aplicarle el estilo
+              divHijoPorTitulo.appendChild(aGuia);
+            }
+            break;
+              
           default:
             break;
         }
@@ -317,6 +340,17 @@ function mostrarLibros(libros) {
             divHijoPorTitulo.appendChild(aEscuchar);
           }
           break;
+        case 6:
+          if (element !== '') {
+            const aGuia = document.createElement('a');
+            aGuia.href = '/leerguia.html?id=' + id;
+            aGuia.target = '_blank';
+            aGuia.innerHTML = '<i class="fa-solid fa-chalkboard-user"></i> Guía';
+            aGuia.classList.add('guia-link'); // Clase para aplicarle el estilo
+            divHijoPorTitulo.appendChild(aGuia);
+          }
+          break;
+        
         default:
           break;
       }
--- Recursos/scripts/libros/read.js
+++ Recursos/scripts/libros/read.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const urlParams = new URLSearchParams(window.location.search);
     const bookId = urlParams.get('id');
     let pdfLibro = '/Recursos/pdfs/' + bookId + '.pdf';
+    let pdfGuia = '/Recursos/pdfs/guias/' + bookId + '.pdf';
 
     const conCategoria = JSON.parse(localStorage.getItem('conCategoria'));
 
@@ -10,8 +11,14 @@ document.addEventListener('DOMContentLoaded', function() {
         const libro = conCategoria.find((fila) => fila[0] === id);
         if (libro) {
             pdfLibro = libro[4];
+            pdfGuia = libro[6];
             console.log('pdfLibro', pdfLibro);
-            mostrarPDF(pdfLibro);
+            console.log('pdfGuia', pdfGuia);
+            if (window.location.pathname.includes('leerlibro.html')) {
+                mostrarPDF(pdfLibro);
+            } else if (window.location.pathname.includes('leerguia.html')) {
+                mostrarPDF(pdfGuia);
+            }
             setupGuardarButton(libro);
             if (libro.length > 5) {
                 const audioUrl = libro[5] || '';
@@ -34,76 +41,117 @@ document.addEventListener('DOMContentLoaded', function() {
     function mostrarPDF(url) {
         const pdfjsLib = window['pdfjs-dist/build/pdf'];
         pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js';
-
+    
         pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
             const pdfViewer = document.getElementById('pdf-viewer');
             pdfViewer.classList.add('pdf-viewer')
             
             pdfViewer.innerHTML = '';
-
+    
             let pageNum = 1;
             const totalPages = pdfDoc_.numPages;
-
+    
             function renderPage(page) {
                 const viewportWidth = window.innerWidth;
             
                 if (viewportWidth < 768) {
-                    scale = 0.45; // Escala para dispositivos móviles
+                    scale = 0.45;
                 } else {
-                    scale = 1.5; // Escala para dispositivos de escritorio
+                    scale = 1.5;
                 }
-
+    
                 const viewport = page.getViewport({ scale: scale });
-
+    
                 const canvas = document.createElement('canvas');
                 const ctx = canvas.getContext('2d');
                 canvas.height = viewport.height;
                 canvas.width = viewport.width;
-
+    
                 const renderContext = {
                     canvasContext: ctx,
                     viewport: viewport
                 };
-
+    
                 page.render(renderContext).promise.then(() => {
                     pdfViewer.appendChild(canvas);
-
+    
                     if (pageNum === 1) {
                         setupScrollProgress(pdfViewer, pdfDoc_, totalPages);
+                        
+                        const lastPage = parseInt(localStorage.getItem(`book-${bookId}-lastPage`)) || 1;
+                        const lastScroll = parseInt(localStorage.getItem(`book-${bookId}-lastScroll`)) || 0;
+                        
+                        if (lastPage > 1) {
+                            pdfDoc_.getPage(lastPage).then(renderPage);
+                        }
+                        
+                        setTimeout(() => {
+                            window.scrollTo(0, lastScroll);
+                        }, 100);
                     }
-
+    
                     if (pageNum < totalPages) {
                         pageNum++;
                         pdfDoc_.getPage(pageNum).then(renderPage);
                     }
                 });
             }
-
+    
             pdfDoc_.getPage(pageNum).then(renderPage);
         });
     }
 
-    // Función para configurar el progreso al hacer scroll
-    function setupScrollProgress(pdfViewer, pdfDoc, totalPages) {
-        const progressBarContainer = document.getElementById('progress-bar-container');
-        const progressBar = document.getElementById('progressBar');
-        const pdfContainer = document.querySelector('.pdf-container');
+//Barra de progreso
+function setupScrollProgress(pdfViewer, pdfDoc, totalPages) {
+    const progressBarContainer = document.getElementById('progress-bar-container');
+    const progressBar = document.getElementById('progressBar');
 
-        window.addEventListener('scroll', function() {
-            const scrollTop = window.scrollY;
-            const documentHeight = pdfViewer.scrollHeight;
-            const windowHeight = window.innerHeight;
-            const scrollProgress = (scrollTop / (documentHeight - windowHeight)) * 100;
-            progressBar.style.width = scrollProgress.toFixed(2) + '%';
-            progressBarContainer.style.display = 'block';
+    function updateProgressBar() {
+        const scrollTop = window.scrollY;
+        const documentHeight = pdfViewer.scrollHeight;
+        const windowHeight = window.innerHeight;
+        const scrollProgress = (scrollTop / (documentHeight - windowHeight)) * 100;
+        const roundedProgress = scrollProgress.toFixed(0);
 
-            if (scrollTop + windowHeight >= documentHeight) {
-                progressBarContainer.style.display = 'none';
-            }
-        });
+        progressBar.style.width = roundedProgress + '%';
+        progressBar.setAttribute('data-progress', roundedProgress + '%');
+        progressBarContainer.style.display = 'block';
+
+        const currentPage = Math.ceil(scrollTop / pdfViewer.clientHeight) + 1;
+        localStorage.setItem(`book-${bookId}-lastPage`, currentPage);
+        localStorage.setItem(`book-${bookId}-lastScroll`, scrollTop);
     }
 
-    document.addEventListener('DOMContentLoaded', setupScrollProgress);
+    window.addEventListener('scroll', updateProgressBar);
+    window.addEventListener('resize', updateProgressBar);
+
+    setTimeout(updateProgressBar, 200);
+}
+
+    //Función para guardar el progreso de lectura
+    function guardarProgresoLectura() {
+        const visorPDF = document.getElementById('pdf-viewer');
+        const scrollTop = window.scrollY;
+        const alturaTotal = visorPDF.scrollHeight - window.innerHeight;
+        const progreso = scrollTop / alturaTotal;
+        
+        localStorage.setItem(`libro-${bookId}-progreso`, progreso);
+    }
+
+    function cargarProgresoLectura() {
+        const progreso = parseFloat(localStorage.getItem(`libro-${bookId}-progreso`)) || 0;
+        const visorPDF = document.getElementById('pdf-viewer');
+        const alturaTotal = visorPDF.scrollHeight - window.innerHeight;
+        const posicionScroll = progreso * alturaTotal;
+        
+        window.scrollTo(0, posicionScroll);
+    }
+
+    setInterval(guardarProgresoLectura, 5000);
+
+    // Llama a cargarProgresoLectura después de que el PDF se haya renderizado
+    // Es posible que necesites ajustar dónde se llama esto según tu lógica de renderizado de PDF
+    document.addEventListener('pdf-renderizado', cargarProgresoLectura);
 
     // Función para el reproductor de audio
     function setupAudioPlayer(audioUrl, isSpreaker, isSpotify) {
